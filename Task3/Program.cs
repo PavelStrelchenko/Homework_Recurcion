5d772be27d1df71f977b3a9fdd794fcff07371a4
Pavel <spavel.engineer@yandex.ru>
2024-06-03 14:47:44 +0300

    Third task

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintNumbers (int [] array, int i)
+{
+    if (i < 0) 
+    {
+        return;
+    }
+    Console.Write($"{array[i]} ");
+    PrintNumbers (array, i - 1);
+    
+}
+int [] numbers = new int [] {1, 2, 3, 4, 5};
+int i = numbers.Length - 1;
+PrintNumbers (numbers, i);
+
