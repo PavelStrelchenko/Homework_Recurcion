c92ffff2ab16817e2c91cd9256fa7f7922e0ed30
Pavel <spavel.engineer@yandex.ru>
2024-06-03 16:35:39 +0300

    First task correction 3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,26 +1,26 @@
 ﻿void NumbersSeries (int m, int n)
 {
-     if (m < n)
-     {
-        if (m == n)
-        {
-        return;
-        }
-        Console.Write(m + " ");
-        NumbersSeries (m + 1, n);
-     }
-     else 
-     {
-        if(m == n)
+    if (m < n)
+    {
+        if (m > n)
         {
             return;
         }
-         Console.Write(m + " ");
-         NumbersSeries (m - 1, n);
-
-     }   
        
-
+    Console.Write(m + " ");
+    NumbersSeries (m + 1, n);
+        
+    }
+    if (m > n)
+    {
+       if (m < n)
+        {
+            return;
+        }
+       
+    Console.Write(m + " ");
+    NumbersSeries (m - 1, n);
+    }
 }
 Console.Write("Введите m: ");
 int m = Convert.ToInt32(Console.ReadLine());
