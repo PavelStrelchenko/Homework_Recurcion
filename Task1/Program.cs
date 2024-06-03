2dd73c50dda2064383200290366f929040bda936
Pavel <spavel.engineer@yandex.ru>
2024-06-03 14:45:39 +0300

    First task

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿void NumbersSeries (int m, int n)
+{
+     if (m < n)
+     {
+        if (m == n)
+        {
+        return ;
+        }
+        Console.Write(m + " ");
+        NumbersSeries (m + 1, n);
+     }
+     else 
+     {
+        if(m == n)
+        {
+            return;
+        }
+        NumbersSeries (m - 1, n);
+        Console.Write(m + " ");
+     }
+       
+
+}
+NumbersSeries (1, 1);
